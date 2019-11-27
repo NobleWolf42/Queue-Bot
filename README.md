ddf63aba8ccf6ac1b9b9dae149f978c5a898cc62
NobleWolf42 <greenboyb1@gmail.com>
2019-11-27 18:43:57 -0500

    updated the code to use discord.js

--- README.md
+++ README.md
@@ -3,10 +3,9 @@ Basic Queue Bot for Discord
 
 If you would like to halp in the development of this bot, the discord is https://discord.gg/tgJtK7f.
 
-Uses discord.io and winston (winston is for a debug logger, unneeded for final code)
+Uses discord.js
 
 Install Commands for Dependancies:
-- npm install discord.io winston –save
-- npm install https://github.com/woor/discord.io/tarball/gateway_v6
+- npm install discord.js
 
 You will have to change some variables for your own use, copy the `config.example.json` to `config.json` and enter your settings.
--- queue.js
+++ queue.js
@@ -1,56 +1,24 @@
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 var fs = require("fs");
 var config = require('./config.json');
+var adminRoleIDs = [];
+var modRoleIDs = [];
 var queue = [];
 var oldqueue = [];
-var member_info = [];
-var role_ids = [];
-var channel_ids = [];
 
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: config.auth.token,
-   autorun: true
-});
+var client = new Discord.Client();
 
-//Include this function at the top of any admin olny commands
-function admincheck () {
-    //Memory Cleanup
-    role_ids = [];
-
-    //Sets server_info to be all info for your server
-    server_info = bot.servers[config.connection.server_id];
-
-    //This pulls the role information required to verify Admin
-    member_info = server_info.members;
-    role = server_info.roles;
-
-    //Makes an array of ids for selected roles
-    for (key in role) {
-
-        for (a in config.connection.admin_roles){
-
-            if (role[key].name === config.connection.admin_roles[a]) {
-                role_ids.push(role[key].id);
-            };
-        };
-    };
-};
-
-//Function for checking roles
-function checkarray (user_roles) {
-
-    for (i = 0; i < role_ids.length; i++) {
-  
-        for (x = 0; x < user_roles.length; x++) {
-
-            if (role_ids[i] === user_roles[x]) {
-                return true;
-            }
-        }
-    }
-    return false;
+//Throws Error if bot's token is not set.
+if (config.auth.token === 'YOUR BOT TOKEN' || config.auth.token === '') {
+    throw new Error("The 'auth.token' property is not set in the config.js file. Please do this!");
 }
+
+//Logs the bot into discord, using it's auth token
+client.login(config.auth.token);
+
+//Logs Errors
+client.on('error', console.error);
 
 //Function that checks to see if changes have been made to the queue, and if they have, saves them to a file
 function saveQueue () {
@@ -60,10 +28,7 @@ function saveQueue () {
     if (oldqueue != queue) {
         
         for (i = 0; i < queue.length; i++) {
-            queueOutput.push({
-                "Name": queue[i].Name,
-                "Message": queue[i].Message
-            });
+            queueOutput.push(queue[i]);
         }
 
         fs.writeFile("savedQueue.json", JSON.stringify(queueOutput), function(err) {
@@ -89,179 +54,192 @@ function loadQueue () {
             var queueInput = JSON.parse(data);
 
             for (i = 0; i < queueInput.length; i++) {
-                queue.push(new User (queueInput[i].Name, queueInput[i].Message));
+                queue.push(queueInput[i]);
             }
         });
     }
 }
 
-// Checks the given channel ID to the allowed channel IDs in our config
-function isChannelAllowed (channelID) {
+//Function that calls the server roles
+function serverRoleUpdate(sRole) {
     
     //Memory Cleanup
-    role_ids = [];
+    adminRoleIDs = [];
+    modRoleIDs = [];
 
-    //Sets server_info to be all info for your server
-    server_info = bot.servers[config.connection.server_id];
+    //Sets Local Varibles
+    var basicServerRoles = {};
 
-    //This pulls the channel information required to verify Admin
-    channels_info = server_info.channels;
+    //Saves the Server Roles to an object by name
+    for (let [key, value] of sRole) {
+        index = value.name;
+        basicServerRoles[index] = key;
+    }
 
-    //Makes an array of ids for selected channels
-    for (key in channels_info) {
+    //Loops throught the Admin Role Names, pusing them to an array
+    for (key in config.general.adminRoles) {
+        
+        //Pushes role IDs to Admin if they Match config.general.adminRoles
+        if (basicServerRoles[config.general.adminRoles[key]]){
+            adminRoleIDs.push(basicServerRoles[config.general.adminRoles[key]]);
+        }
+    }
 
-        for (a in config.connection.allowed_channels){
+    //Loops throught the Mod Role Names, pusing them to an array
+    for (key in config.general.modRoles) {
+        
+        //Pushes role IDs to Mods if they Match config.general.modRoles
+        if (basicServerRoles[config.general.modRoles[key]]){
+            modRoleIDs.push(basicServerRoles[config.general.modRoles[key]]);
+        }
+    }
+}
 
-            if (channels_info[key].name === config.connection.allowed_channels[a]) {
-                channel_ids.push(channels_info[key].id);
-            };
-        };
-    };
-   
-    //Returns True if the channel posted in is one of the channels in config.connection.allowed_chanels
-    if(channel_ids.indexOf(channelID) != -1) { 
-        return true;
+//Function that returns boolean for if the user who sent the message is an Admin (based off config.connection.adminRoles)
+function adminCheck(userRolesArray, serverRolesArray) {
+    
+    //Calls a function that updates the server role information
+    serverRoleUpdate(serverRolesArray);
+    
+    //Checks to see if any of the user role ids match any of the admin role ids
+    for (key in userRolesArray) {
+        
+        for (a in adminRoleIDs) {
+
+            if (userRolesArray[key] == adminRoleIDs [a]) {
+
+                return true;
+            }
+        }
     }
 
     return false;
 }
 
-// Sets the Status Message of the bot (i.e. when a user is "Playing Sea Of Thieves")
-bot.on('ready', function(evt) {
-    bot.setPresence( {game: {name: "*help"}} );
-    loadQueue;
-    setInterval(saveQueue, 600000);
+//Function that returns boolean for if the user who sent the message is a Moderator (based off config.connection.modRoles)
+function modCheck(userRolesArray, serverRolesArray) {
+    
+    //Calls a function that updates the server role information
+    serverRoleUpdate(serverRolesArray);
+    
+    //Checks to see if user role ids match any of the mod role ids
+    for (key in userRolesArray) {
+        
+        for (a in modRoleIDs) {
 
-    //Info code for minor Debugging
-    /*for (key in bot.servers[config.connection.server_id].channels) {
-        console.log(bot.servers[config.connection.server_id].channels[key]);
-    };*/
+            if (userRolesArray[key] == modRoleIDs [a]) {
+
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+//Logs the Bot info when bot starts
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({ game: { name: `Use ${config.general.botPrefix}help to show commands` } });
+    loadQueue();
+    setInterval(saveQueue, 600000);
 });
 
-// Listens to Messages and executes various commands
-bot.on('message', function (user, userID, channelID, message, evt) {
+//Handels Messages and their responses
+client.on("message", message => {
+    
+    if (config.general.allowedChannels.indexOf(message.channel.name) != -1) {
 
-    // The bot will listen for messages that will start with `*`, in the channels in config.connection.allowed_channels
-    if ((message.substring(0, 1) == '*' && isChannelAllowed(channelID))) {
-        var args = message.substring(1).split(', ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
+        //Varibles for the message info needed
+        var userInput = message.content.toLowerCase();
+        var userRoles = message.author.lastMessage.member._roles;
+        var serverRoles = message.channel.guild.roles;
+        var adminTF = adminCheck(userRoles, serverRoles);
+        var modTF = modCheck(userRoles, serverRoles);
+
+        // join, adds the user who sent the message to an Array containing the Queue list
+        if (userInput == (config.general.botPrefix + 'join')){
         
-        // All commands go here
-        switch(cmd) {
-            
-            // *join, adds the user who sent the message to an Array containing the Queue list
-            case 'join':
-                if (queue.indexOf(String(user + "#" + bot.users[userID].discriminator)) === -1) {
-                    queue.push(String(user + "#" + bot.users[userID].discriminator));
-                    location = queue.indexOf(String(user + "#" + bot.users[userID].discriminator));
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `You are at position ${location + 1} in the queue!`
-                    });
-                }
-                else {
-                    location = queue.indexOf(String(user + "#" + bot.users[userID].discriminator));
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `You are already at position ${location + 1} in the queue!`
-                    });
-                }
-            break;
+            location = queue.indexOf(String(message.member.user.tag));
 
-            // *leave, removes the user who sent the message from the Queue list
-            case 'leave':
-                queue.splice(queue.indexOf(String(user + "#" + bot.users[userID].discriminator)), 1);
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'You have left the queue!'
-                });
-            break;
+            if (location === -1){
+                queue.push(String(message.member.user.tag));
+                location = queue.indexOf(String(message.member.user.tag));
+                message.reply(`You have joined at position ${location + 1} in the queue!`);
+            }
+            else {
+                message.reply(`You are already at position ${location + 1} in the queue!`);
+            }
+        };
 
-            // *queue, displays the current Queue list
-            case 'queue':
-                arrtext = "";
-                msgtxt = "";
-                arrlen = queue.length;
+        // leave, removes the user who sent the message from the Queue list
+        if (userInput == (config.general.botPrefix + 'leave')){
+            queue.splice(String(message.member.user.tag), 1);
+            message.reply("You have left the queue.");
+        };
 
-                for (i = 0; i < arrlen; i++) {
-                    arrtext += (i + 1) + ' - ' + queue[i] + "\n";
-                }
+        // position, returns the current queue position for the user who sent the message from the Queue list
+        if (userInput == (config.general.botPrefix + 'position')){
+            location = queue.indexOf(String(message.member.user.tag));
 
-                if (arrtext === ""){
-                    arrtext = "Queue is Currently Empty."
-                }
+            if (location === -1){
+                message.reply(`You are not currently in the queue!`);
+            }
+            else {
+                message.reply(`You are at position ${location + 1} in the queue!`);
+            }
+        };
 
-                msgtxt = '```' + arrtext + '```';
-                bot.sendMessage({
-                    to: channelID,
-                    message: `${msgtxt}`
-                });
-            break;
+        // queue, displays the current Queue list
+        if (userInput == (config.general.botPrefix + 'queue')){
+            arrtext = "";
+            msgtxt = "";
 
-            // *remove, "USERNAME", Server Admin/Creator only command that removes the user specified from the Queue list
-            case 'remove':
-                admincheck();
+            for (i = 0; i < queue.length; i++) {
+                arrtext += (i + 1) + ' - ' + queue[i] + "\n";
+            }
 
-                if(checkarray(member_info[userID].roles)) {
+            if (arrtext === ""){
+                arrtext = "Queue is Currently Empty."
+            }
 
-                    if (queue.indexOf(String(args)) == -1) {
-                        bot.sendMessage({
-                            to: channelID,
-                            message: `You did not specify a valid user.`
-                        });
-                    }
-                    else {
-                        queue.splice(queue.indexOf(String(args)), 1);
-                        bot.sendMessage({
-                            to: channelID,
-                            message: `Removed ${args} from Queue.`
-                        });
-                    }
-                }
-                else {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `You do not have permission to use this command.`
-                    });
-                }
-            break;
+            msgtxt = '```' + arrtext + '```';
+        
+            message.channel.send(msgtxt);
+        };
 
-            // *clearqueue, Server Admin/Creator only command that clears the entire Queue list
-            case 'clearqueue':
-                admincheck();
-                
-                if(checkarray(member_info[userID].roles)) {
-                    queue = ['Test'];
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `Cleared Queue.`
-                    });
-                }
-                else {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `You do not have permission to use this command.`
-                    });
-                }
-            break;
+        // help, Lists all commands and what they do
+        if (userInput == (config.general.botPrefix + 'help')){
+            var rply = '```' + config.general.botPrefix + 'help - Displays All Commands\n' + config.general.botPrefix + 'join - Adds you to the Queue to get into a ship\n' + config.general.botPrefix + 'leave - Removes you from the Queue to get into a ship\n' + config.general.botPrefix + 'position - Displays your current position in the Queue list\n' + config.general.botPrefix + 'queue - Displays the current Queue list\n' + config.general.botPrefix + 'remove, USERNAME - Server Admin/Creators only caommnad that removes the specified user from the Queue List\n' + config.general.botPrefix + 'clearqueue - Server Admin/Creators only command that clears the entire Queue list\n' + config.general.botPrefix + 'info - Information about the bot and its creators.```';
+            message.channel.send(rply);
+        };
 
-            // *help, Lists all commands and what they do
-            case 'help':
-                bot.sendMessage({
-                    to:channelID,
-                    message: '```*help - Displays All Commands\n*join - Adds you to the Queue to get into a ship\n*leave - Removes you from the Queue to get into a ship\n*queue - Displays the current Queue list\n*remove, USERNAME - Server Admin/Creators only caommnad that removes the specified user from the Queue List\n*clearqueue - Server Admin/Creators only command that clears the entire Queue list\n*info - Information about the bot and its creators.```'
-                })
-            break;
+        // info, returns info message
+        if (userInput == (config.general.botPrefix + 'info')){
+            message.channel.send('```Queue System Bot\nQueue system for Sea of Thieves Fleet/Alliance Servers\nDesigned and Bulit by: NobleWolf42 and DK1\nIf you would like to assist with the bot, you can find us on Discord at https://discord.gg/tgJtK7f, and on GitHub at https://github.com/NobleWolf42/Queue-Bot/.```');
+        };
 
-            // *info, Lists all commands and what they do
-            case 'info':
-                bot.sendMessage({
-                    to:channelID,
-                    message: '```Queue System Bot\nQueue system for Sea of Thieves Fleet/Alliance Servers\nDesigned and Bulit by: NobleWolf42 and DK1\nIf you would like to assist with the bot, you can find us on Discord at https://discord.gg/tgJtK7f, and on GitHub at https://github.com/NobleWolf42/Queue-Bot/.```'
-                })
-            break;
-         }
-     }
+        //ADMIN ONLY COMMANDS
+        // clearqueue, Server Admin/Creator only command that clears the entire Queue list
+        if ((adminTF == true) && (userInput == (config.general.botPrefix + 'clearqueue'))){
+            if (adminTF == true){
+                queue = [];
+                message.reply("Cleared Queue");
+            }
+            else {
+                message.channel.send("You do not have permission to use this command.");
+            }
+        };
+
+        // remove @USER, Server Admin/Creator only command that removes the user specified from the Queue list
+        if (userInput.slice(0,7) == (config.general.botPrefix + 'remove')){
+            if (adminTF == true){
+                queue.splice(queue.indexOf(String(message.mentions.users.first().tag)), 1);
+                message.reply(`Removed ${message.mentions.users.first().tag} from Queue.`);
+            }
+            else {
+                message.channel.send("You do not have permission to use this command.");
+            }
+        };
+    };
 });
